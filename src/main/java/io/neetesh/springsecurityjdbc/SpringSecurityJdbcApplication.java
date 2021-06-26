92834216c9f397126683000c8556bdabf08de2f6
neeteshtmrkr <tamrakarnitesh21@gmail.com>
2021-06-26 22:19:27 +0545

    Some changes made in build.gradle,application.properties

--- src/main/java/io/neetesh/springsecurityjdbc/SpringSecurityJdbcApplication.java
+++ src/main/java/io/neetesh/springsecurityjdbc/SpringSecurityJdbcApplication.java
@@ -1,9 +1,12 @@
 package io.neetesh.springsecurityjdbc;
 
+import io.neetesh.springsecurityjdbc.Repo.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class SpringSecurityJdbcApplication {
 
     public static void main(String[] args) {
