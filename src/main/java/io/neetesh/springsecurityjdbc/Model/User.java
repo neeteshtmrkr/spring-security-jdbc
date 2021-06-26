2e27d2e1acd454516990a3c0921cfc4bf73ba49d
neeteshtmrkr <tamrakarnitesh21@gmail.com>
2021-06-26 22:17:31 +0545

    Added model,repo,security and services

---
+++ src/main/java/io/neetesh/springsecurityjdbc/Model/User.java
@@ -0,0 +1,48 @@
+package io.neetesh.springsecurityjdbc.Model;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name ="User_table")
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String userName;
+    private String password;
+    private String roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+}
---
+++ src/main/java/io/neetesh/springsecurityjdbc/Services/MyUserDetailsService.java
@@ -0,0 +1,28 @@
+package io.neetesh.springsecurityjdbc.Services;
+
+import io.neetesh.springsecurityjdbc.Model.MyUserDetails;
+import io.neetesh.springsecurityjdbc.Model.User;
+import io.neetesh.springsecurityjdbc.Repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+       Optional<User> user = userR epository.findByUserName(userName);
+
+       user.orElseThrow(()->new UsernameNotFoundException("Not Found: "+userName));
+
+       return user.map(MyUserDetails::new).get();
+    }
+}
